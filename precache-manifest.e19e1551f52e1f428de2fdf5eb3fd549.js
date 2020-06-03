40bdda28e74bb44255c5cf483d24ae7b9e2144d4
sibashish biswas <sibashishbiswas349@gmail.com>
2020-06-03 19:33:10 +0530

    Updates

---
+++ precache-manifest.e19e1551f52e1f428de2fdf5eb3fd549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd7a0e16c421a8e87db05e2e7c754ac",
+    "url": "/React_countdown/index.html"
+  },
+  {
+    "revision": "5ad988ab5e347273266d",
+    "url": "/React_countdown/static/css/main.36e35a43.chunk.css"
+  },
+  {
+    "revision": "95c992d1759b6b6e56aa",
+    "url": "/React_countdown/static/js/2.44fad1d0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_countdown/static/js/2.44fad1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad988ab5e347273266d",
+    "url": "/React_countdown/static/js/main.749d81cf.chunk.js"
+  },
+  {
+    "revision": "96a8bfd10b62ee7b5565",
+    "url": "/React_countdown/static/js/runtime-main.33b0e92f.js"
+  }
+]);
